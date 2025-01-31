3fef15a2afe3aa1cfcb0db97cb65304070aec394
aanandhisonduri <aanandhis07@gmail.com>
2025-01-31 09:46:25 -0800

    auth file

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://dynamic-badger-92.clerk.accounts.dev/ ",
+        domain: "https://dynamic-badger-92.clerk.accounts.dev/",
         applicationID: "convex",
       },
     ]
